f3df69c3ee9f7c0b2b7f9ec45f91752e0a7c6302
Anjali <anjali33100@gmail.com>
2022-09-21 14:41:12 +0530

    first commit

---
+++ get_col_details.py
@@ -0,0 +1,16 @@
+import pandas as pd
+df= pd.read_csv(r'C:\Users\Anjali_SEQ\Downloads\2022.09.21 07.25.38Z_autotrader.csv')
+
+Total_count_of_0_col = df[df.columns[0]].count()
+value = []
+list_string = ""
+column_names = df.columns
+for i in column_names:
+    percent = ((df[i].count())/Total_count_of_0_col) * 100
+    value.append("%.2f"% percent)
+    list_string += i + ","
+
+col_name = list_string[:-1]
+colwise = dict(zip(column_names, value))
+
+print("column_names : ",col_name,"\n\n","Column wise Percentage : ",colwise,"\n\n","Number of column : ",len(df.axes[1]))
