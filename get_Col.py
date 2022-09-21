b1e1260526b1436d13400bed21a4926ad7c7aa95
Anjali <anjali33100@gmail.com>
2022-09-21 13:02:15 +0530

    first commit

--- get_Col.py
+++ get_Col.py
@@ -1,13 +1,12 @@
 import pandas as pd
 df= pd.read_csv(r'C:\Users\Anjali_SEQ\Desktop\milled_Test_20220920_003504.csv')
 
-
-
+Total_count_of_0_col = df[df.columns[0]].count()
 value = []
 list_string = ""
 column_names = df.columns
 for i in column_names:
-    percent = ((df[i].count())/5744) * 100
+    percent = ((df[i].count())/Total_count_of_0_col) * 100
     value.append("%.2f"% percent)
     list_string += i + ","
 
