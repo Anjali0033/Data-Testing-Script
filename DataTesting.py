d8045475eca334465c3bc4bd9cc79cde4fe7fa30
Anjali <anjali33100@gmail.com>
2022-09-27 00:04:17 +0530

    first commit

--- DataTesting.py
+++ DataTesting.py
@@ -5,32 +5,16 @@ import pandas as pd
 import json
 
 
+# Data testing for flightclub_newRelease_neustreet
 
-
-agent_name_input = "pcpartpicker"
-data_file = pd.read_csv(r"C:\Users\Anjali_SEQ\Downloads\2022-09-19T22.45.35Z_pcpartpicker.csv",encoding='utf-8-sig') #read input file
+agent_name_input = "careers_aldi"
+data_file = pd.read_csv(r"C:\Users\Anjali_SEQ\Downloads\careers_aldi_20220920_084020.csv",encoding='utf-8-sig') #read input file
 table_details = pd.read_csv(r"C:\Users\Anjali_SEQ\Downloads\Details_for_Automation - Citadel Survey Capital.csv", encoding= 'unicode_escape') #read input file
+column_name = ["RunDate","RUNDATE","RUN_DATE","LoadDate","LOADDATE","SCRAPE_DATE","Run Date","Rundate","run_date","Date","scrape_date"]
 
 
 def parsetolist(head):
-    if "," in head:
-        return head.split(',')
-    else:
-        list = [head]
-        return list
-
-def  correct_rundateCol():
-    column_name = ["RunDate","RUNDATE","RUN_DATE","LoadDate","LOADDATE","SCRAPE_DATE","Run Date"]
-    rundateCol = ""
-    for File in range(len(data_file)):
-        for column in column_name:
-            try :
-                data_file[column].iloc[File]            
-                rundateCol = column
-                break
-            except:
-                pass
-    return rundateCol
+    return head.split(',')
 
 class DataTesting:
 
@@ -48,114 +32,115 @@ class DataTesting:
         
         self.colNames = list(data_file.head())
 
-# ------------------------------------------------------------------------------
-    def print_(self):
-        print((type()))
-        print(self.colWiseCount)
-        print(type(json.loads(self.colWiseCount)))
-
-# ------------------------------------------------------------------------------
 
     def validateColumns(self):
-        #Providing column names of data file.
-        try : 
-            ColumnsNames = parsetolist(self.ColumnsNames)
 
-            errors = []
+        ColumnsNames = parsetolist(self.ColumnsNames)
+        errors = []
 
-            if len(self.file.columns) != int(self.colCount):
-                errors.append("Column Count Is Incorrect")
+        if len(self.file.columns) != int(self.colCount):
+            errors.append("Column Count Is Incorrect")
 
-            if list(self.file.columns) != ColumnsNames:
-                errors.append("Sequence Is Incorrect")
+        if list(self.file.columns) != ColumnsNames:
+            errors.append("Sequence Is Incorrect")
 
-            if len(errors):
-                return errors
+        if len(errors):
+            return errors
 
-            return "Column Count And Sequence is Correct"
-
-        except:
-
-            return "ColumnsNames is not available in agent file"
+        return "Column Count And Sequence is Correct" 
 
 
     def validateDataTime(self):
-    ##     # Validate RunDate Column
-        Rundate = ""
-        for file_col in ((self.file).columns).tolist():
-            for list_col in column_name:
-                if file_col == list_col:
-                    Rundate = file_col
-                    break
-        print(Rundate)
+    ##  # Validate RunDate Column
+        try:
+            Rundate = ''
+            for file_col in self.file.columns.tolist():
+                for list_col in column_name:
+                    # print(file_col,list_col)
+                    if file_col == list_col:
+                        Rundate = file_col
+                        break
+            if Rundate is not None:
+                runDateResult = ""
 
-        # runDateResult = ""
-        # for rundate in self.file[Rundate].unique():
-            # if type(rundate) != str:
-                # continue
-            # 
-            # if datetime.strftime(parse(rundate).date(),"%Y-%m-%d") and time.strftime(parse(rundate).time(),'%H:%M:%S') and parse(rundate).tzinfo != None:    
-                # runDateResult = "{} Format is correct",format(Rundate)
-            # else:
-                # runDateResult = "{} Incorrect - {}".format(Rundate,rundate)
-                # break
-# 
-            # return(runDateResult)
+                for rundate in self.file[Rundate].unique():
+                    if type(rundate) != str:
+                        continue
+                    
+                    if datetime.strftime(parse(rundate).date(),"%Y-%m-%d") and time.strftime(parse(rundate).time(),'%H:%M:%S') and parse(rundate).tzinfo != None:    
+                        runDateResult = f"Rundate Format is correct"
+                    else:
+                        runDateResult = f"{Rundate} Incorrect Rundate - {rundate}"
+                        break
+                    
 
-        ## Validate RunTime Column
-        #runTimeResult = ""
-        #for DataFile in range(len(self.file)):
-        #    runtime = self.file['rundate_time'].iloc[DataFile]
-#
-        #    if type(runtime) != str:
-        #        continue
-        #    
-        #    if datetime.strftime(parse(runtime).date(),"%d-%m-%Y") and time.strftime(parse(runtime).time(),'%H:%M:%S'):    
-        #        runTimeResult = "RunTime Format is correct"
-        #    else:
-        #        runTimeResult = "RunTime Incorrect"
-        #        break
-#
-        #return [runDateResult,runTimeResult]
+                ## Validate RunTime Column
+                runTimeResult = ""
+                for DataFile in range(len(self.file)):
+                   runtime = self.file[Rundate].iloc[DataFile]
+
+                   if type(runtime) != str:
+                       continue
+                   
+                   if datetime.strftime(parse(runtime).date(),"%d-%m-%Y") and time.strftime(parse(runtime).time(),'%H:%M:%S'):    
+                        runTimeResult = "Runtime Format is correct"
+                   else:
+                        runTimeResult = "{} Incorrect Runtime - {}".format(Rundate,rundate)
+                        break
+        #       
+                return runDateResult,runTimeResult + f" For : {Rundate}(column name)"
+
+            else : 
+                return f"Please update column_name of rundate for {agent_name_input} Agent.(To update this , You will find column_name list variable With in script.)"
+        except:
+
+            return "Some Unexpected Error in validateDataTime Function."
+
 
     def validateUniqueCol(self):
         try:
-            null = []
-
+            notunique = []
             for col in parsetolist(self.UniqueCol):
-                if self.file[self.UniqueCol].is_unique:
-                    null.append(col)
+                if self.file[col].is_unique:
+                    pass
+                else:
+                    notunique.append(col)
 
-            if len(null):
-                return f"No Fields Are dublicate Which Are Set To Be Unique : {parsetolist(self.UniqueCol)}"
-            return f"These Fields Are Set To Be Unique, but Still Containing dublicate Values : {null} ."
+
+                if len(notunique):
+                    return f"These Fields Are Set To Be Unique, Still Containing dublicate Values : {notunique} ."
+            
+                return f"No Fields Are dublicate Which Are Set to be unique : {parsetolist(self.UniqueCol)}"  
 
         except:
-
-            return "Unique columns are not available in agent file"
-
+            if type(self.nonNull) == float:
+                return "{} Agent have no Unique fields .".format(agent_name_input)
+            else:
+                return "Some Unexpected error in validateUniqueCol function."
 
 
     def validateNonNullColumns(self):
-    
+
         try:
             columnPerc = dict(self.file[self.file.columns[self.file.notnull().any()]].notnull().sum() * 100 / self.file.shape[0])
             
             # print(columnPerc.keys())
-            # print(parsetolist(self.nonNull))
             null = []
             for col in parsetolist(self.nonNull):
                 if columnPerc[col] != 100.0:
                     null.append(col)
-    # 
+
             if len(null):
                 return f"These Fields Are Set To Be Not Null, Still Containing Null Values : {null} ."
-
-            return "No Fields Are Blank Which Are Set To Be Not Null"
         
+            return "No Fields Are Blank Which Are Set To Be Not Null "
+
         except:
-            
-            return "Not Null columns are not available in agent file"
+            if type(self.nonNull) == float:
+                return "{} Agent have no NotNull fields are available.".format(agent_name_input)
+            else:
+                return "Some unexpected error facing in validateNonNullColumns function."
+
 
 
     def validateCategoryWiseCount(self):
@@ -173,15 +158,14 @@ class DataTesting:
             if len(less):
                 return f"Category Wise Count Is Not Correct : {less} ."
                 
-            return "Category Wise Count is Correct"
-        
+            return "Category Wise Count is Correct "
+
         except:
-            return "Category wise column is Not available"
+            return "Some Unexpected Error in validateCategoryWiseCount function."
 
 
     def validateCompleteBlankCol(self):
         try:
-                
             errors = []
             for col in self.colNames:
                 result = False
@@ -203,22 +187,24 @@ class DataTesting:
             if len(errors):
                 return f"These Are Completely Blank Columns : {errors}"
 
-            return "No Completely Blank Columns Are Available in result file."
-        
-        except:
+            return "No Completely Blank Columns Are Available ."
 
-            return "complete blank column is Not available in agent"
+        except:
+            return "Some unexceptected error in validateCompleteBlankCol Function."
+        
 
 
 datafile = DataTesting(data_file)
 
-print(datafile.validateColumns())
-# print(datafile.validateDataTime())
-print(datafile.validateUniqueCol()) 
-print(datafile.validateNonNullColumns())
-print(datafile.validateCategoryWiseCount())
-print(datafile.validateCompleteBlankCol())
+Report = 70*'-' + f"\nData Testing report for {agent_name_input} agent\n"
 
-# Report = f"1.{datafile.validateColumns()}\n\n3.{datafile.validateNonNullColumns()}\n4.{datafile.validateCategoryWiseCount()}\n5.{datafile.validateCompleteBlankCol()}"
-# print(Report)
-# 
+# print(datafile.validateColumns())
+# print(datafile.validateDataTime())
+# print(datafile.validateUniqueCol()) 
+# print(datafile.validateNonNullColumns())
+# print(datafile.validateCategoryWiseCount())
+# print(datafile.validateCompleteBlankCol())
+#
+
+Report = Report + f"1.{datafile.validateColumns()}\n2.{datafile.validateDataTime()}\n3.{datafile.validateNonNullColumns()}\n4.{datafile.validateCategoryWiseCount()}\n5.{datafile.validateCompleteBlankCol()}"
+print(Report)
